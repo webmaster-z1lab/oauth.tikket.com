2d08d4f579ab2ea568f1fadba2506714c88554df
Olimar Ferraz <olimarferraz@gmail.com>
2018-12-13 12:34:04 -0200

    Register TTL middleware;

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'throttle'      => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified'      => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'api.v'         => \Juampi92\APIResources\Middleware\APIversion::class,
+        'ttl'           => \Barryvdh\HttpCache\Middleware\SetTtl::class,
     ];
 
     /**
